a26bb56c93fbc558e6cea5b56fb29c63b9b35aa2
Tom Morrell <tmorrell@users.noreply.github.com>
2025-10-11 13:48:07 -0700

    Add build script
    Removed unused directories and flags related to dummy data.

---
+++ _build.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import subprocess
+import argparse
+
+outDir = "site"
+stylesDir = "styles"
+imagesDir = "img"
+
+def buildPage(page, template):
+    cmd = []
+    cmd.append("pandoc")
+    cmd.append("--from=markdown")
+    cmd.append("--to=html")
+    cmd.append(f"--output={outDir}/{page}.html")
+    cmd.append(f"--template=templates/{template}.html")
+
+    if (args.dummy): cmd.append(f"--metadata=dummy")
+
+    cmd.append(f"pages/{page}.md")
+
+    subprocess.run(cmd)
+
+argParser = argparse.ArgumentParser()
+args = argParser.parse_args()
+
+# Create output site directory
+if os.path.isdir(outDir): shutil.rmtree(outDir)
+os.makedirs(outDir)
+shutil.copytree(imagesDir, f"{outDir}/{imagesDir}")
+shutil.copytree(stylesDir, f"{outDir}/{stylesDir}")
+
+
+if args.dummy: shutil.copytree("dummy", f"{outDir}/{dummyDataDir}")
+
+buildPage("index", "index")
